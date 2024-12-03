9131b494b0b7aea3eed809b119233455076b8de7
codinganiketdatta <2004linuxsudoaniketdatta@gmail.com>
2024-12-03 15:26:29 +0000

    ok

--- zentry/src/App.jsx
+++ zentry/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [count, setCount] = useState(0)
   return (
 <main className='bg-zinc-500 h-screen w-full'>
-  <h1 className='text-5xl text-orange-500 font-bold'>Zentry</h1>
+  <h1 className='text-5xl text-orange-500 font-bold'></h1>
 </main>
   )
 }
